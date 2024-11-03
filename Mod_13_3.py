a64836f8f3305935e58b50c0448c366477a22e9c
Dmitry <prrtaf@yandex.ru>
2024-11-03 13:14:05 +0300

    Created

---
+++ Mod_13_3.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+API = '*your_API_must_be_here*'
+botik = Bot(token=API)
+dp = Dispatcher(botik, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(msg):
+    await msg.answer("Hi! I'm a health helper bot")
+
+
+@dp.message_handler()
+async def all_msgs(msg):
+    await msg.answer('Use /start command to talk')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
